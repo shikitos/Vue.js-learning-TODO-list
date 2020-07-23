1bb0fef53edb7d5c135accc0debb85aa24935d47
Nikita Shitov <56273378+shikitos@users.noreply.github.com>
2020-07-23 23:52:03 +0500

    Fix for eslint. Start with localStorage

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,29 @@
 <template>
-  <div id="app">
+  <div id='app'>
     <h1>Todo application</h1>
     <AddTodo
-      @add-todo="addTodo"
+      @add-todo='addTodo'
     />
-    <select v-model="filter">
-      <option value="all" class="">All</option>
-      <option value="completed" class="">Completed</option>
-      <option value="not-completed" class="">Not completed</option>
-    </select>
+    <label>
+      <select v-model='filter'>
+        <option value='all' class=''>All</option>
+        <option value='completed' class=''>Completed</option>
+        <option value='not-completed' class=''>Not completed</option>
+      </select>
+    </label>
     <hr>
     <TodoList
-      v-if="filteredTodos.length"
-      v-bind:todos="filteredTodos"
-      @remove-todo="removeTodo"
+      v-if='filteredTodos.length'
+      v-bind:todos='filteredTodos'
+      @remove-todo='removeTodo'
       />
     <p v-else>No todos...</p>
   </div>
 </template>
 
 <script>
-import TodoList from "./components/TodoList";
-import AddTodo from "./components/AddTodo";
+import TodoList from './components/TodoList.vue';
+import AddTodo from './components/AddTodo.vue';
 
 export default {
   name: 'App',
@@ -29,44 +31,48 @@ export default {
     return {
       todos: [
         {
-          id: 1, title: "Купить молоко", completed: false
+          id: 1, title: 'Купить молоко', completed: false,
         },
         {
-          id: 2, title: "Купить хлеб", completed: false
+          id: 2, title: 'Купить хлеб', completed: false,
         },
         {
-          id: 3, title: "Купить pivo", completed: false
-        }
+          id: 3, title: 'Купить pivo', completed: false,
+        },
       ],
-      filter: 'all'
-    }
+      filter: 'all',
+    };
   },
   components: {
-    TodoList, AddTodo
+    TodoList, AddTodo,
   },
   methods: {
     removeTodo(id) {
-      this.todos = this.todos.filter(t => t.id !== id);
+      this.todos = this.todos.filter((t) => t.id !== id);
     },
     addTodo(todo) {
-      this.todos.push(todo)
-    }
+      this.todos.push(todo);
+      localStorage.setItem('todos', todo);
+    },
   },
   computed: {
     filteredTodos() {
       if (this.filter === 'all') {
         return this.todos;
-      } else if (this.filter === 'completed') {
-        return this.todos.filter(t => t.completed);
-      } else if (this.filter === 'not-completed') {
-        return this.todos.filter(t => !t.completed);
       }
+      if (this.filter === 'completed') {
+        return this.todos.filter((t) => t.completed);
+      }
+      if (this.filter === 'not-completed') {
+        return this.todos.filter((t) => !t.completed);
+      }
+      return true;
     },
   },
 };
 </script>
 
-<style lang="scss">
+<style lang='scss'>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
